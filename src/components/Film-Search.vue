0780c1cd1fcb4bccb92659b62ce9a4ea5aa9ac4a
陈主润 <32399723@qq.com>
2017-06-09 14:18:16 +0800

    finish search page

--- src/components/Film-Search.vue
+++ src/components/Film-Search.vue
@@ -1,44 +1,81 @@
 <template>
   <div>
-    <search ></search>
+    <search @on-submit="searchFilm" @on-change="getResult" v-model="searchFilmName" :results="results" @result-click="resultClick" position="absolute"></search>
+
+    <div class="hot-search-list">
+      <flexbox>
+        <flexbox-item v-for="film in filmName" @click.native="filmDetail(film.id)">
+          <x-button mini plain >{{film.name}}</x-button>
+        </flexbox-item>
+      </flexbox>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import { Search } from 'vux'
+import { Search, XButton, Group, Flexbox, FlexboxItem } from 'vux'
 
 export default {
   components: {
-    Search
+    Search,
+    XButton,
+    Group,
+    Flexbox,
+    FlexboxItem
   },
   data () {
     return {
-      msg: 'Hello World!',
-      usrname: '',
-      psword: ''
+      filmName: [
+        {
+          id: 1,
+          name: '摔跤吧！爸爸'
+        },
+        {
+          id: 2,
+          name: '加勒比海盗5'
+        },
+        {
+          id: 3,
+          name: '神奇女侠'
+        }
+      ],
+      searchFilmName: '',
+      results: []
     }
   },
   methods: {
-    gotoRegister: function () {
-      console.log('executing')
-      window.location = '#register'
+    filmDetail: function (id) {
+      window.location = '#filmDetail/' + id
     },
-    isValid: function () {
-      console.log('validating the form data')
+
+    searchFilm: function () {},
+
+    getResult: function (val) {
+      let rs = []
+      for (let i = 0; i < this.filmName.length; i++) {
+        if (this.filmName[i].name.indexOf(val) >= 0) {
+          rs.push({
+            title: this.filmName[i].name,
+            filmId: this.filmName[i].id,
+            other: ''
+          })
+        }
+      }
+      this.results = rs
     },
-    login: function () {
-      window.location = '#filmson'
+
+    resultClick: function (item) {
+      window.location = '#filmDetail/' + item.filmId
     }
   }
 }
+
 </script>
 
 <style>
-.vux-demo {
-  text-align: center;
-}
-.logo {
-  width: 100px;
-  height: 100px
+.hot-search-list {
+  margin-top: 10px;
+  padding: 5px;
 }
 </style>
